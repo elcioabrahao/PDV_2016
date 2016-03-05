7215b78be0432635269e14c934ba1a06057b87e4
Elcio Abrahão <elcio.abrahao@gmail.com>
2016-03-05 10:42:06 -0300

    Atualização até 10:41

--- app/src/main/java/br/com/trainning/pdv_2016/ui/MainActivity.java
+++ app/src/main/java/br/com/trainning/pdv_2016/ui/MainActivity.java
@@ -1,13 +1,17 @@
-package br.com.trainning.pdv_2016;
+package br.com.trainning.pdv_2016.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.com.trainning.pdv_2016.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,7 +47,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_novo) {
+
+            Intent telaCadastroNovoIntent = new Intent(MainActivity.this,CadastroNovoActivity.class);
+            startActivity(telaCadastroNovoIntent);
+
+        }else if(id == R.id.action_edit){
+            Log.d("MainActivity","Selecionou editar Produto");
             return true;
         }
 
