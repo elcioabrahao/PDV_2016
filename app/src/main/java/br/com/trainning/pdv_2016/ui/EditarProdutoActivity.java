2db8daf93fcdf8664cf25c056bc1ef657e65c95c
Elcio Abrahão <elcio.abrahao@gmail.com>
2016-03-12 10:41:33 -0300

    Nova tela de editar produto

--- app/src/main/java/br/com/trainning/pdv_2016/ui/EditarProdutoActivity.java
+++ app/src/main/java/br/com/trainning/pdv_2016/ui/EditarProdutoActivity.java
@@ -1,6 +1,11 @@
 package br.com.trainning.pdv_2016.ui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,21 +14,47 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.trainning.pdv_2016.R;
 import br.com.trainning.pdv_2016.domain.model.Produto;
+import br.com.trainning.pdv_2016.domain.util.Base64Util;
+import br.com.trainning.pdv_2016.domain.util.ImageInputHelper;
 import butterknife.Bind;
+import butterknife.OnClick;
 import se.emilsjolander.sprinkles.CursorList;
 import se.emilsjolander.sprinkles.Query;
 
-public class EditarProdutoActivity extends BaseActivity {
+public class EditarProdutoActivity extends BaseActivity implements ImageInputHelper.ImageActionListener{
 
     @Bind(R.id.spinner)
     Spinner spinner;
+    @Bind(R.id.editTextDescricao)
+    EditText editTextDescricao;
+    @Bind(R.id.editTextUnidade)
+    EditText editTextUnidade;
+    @Bind(R.id.editTextPreco)
+    EditText editTextPreco;
+    @Bind(R.id.editTextCodigo)
+    EditText editTextCodigo;
+
+    @Bind(R.id.imageViewFoto)
+    ImageView imageViewFoto;
+    @Bind(R.id.imageButtonCamera)
+    ImageButton imageButtonCamera;
+    @Bind(R.id.imageButtonGaleria)
+    ImageButton imageButtonGaleria;
+
+    private ImageInputHelper imageInputHelper;
+
     private Produto produto;
 
     @Override
@@ -33,12 +64,27 @@ public class EditarProdutoActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imageInputHelper = new ImageInputHelper(this);
+        imageInputHelper.setImageActionListener(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                produto.setDescricao(editTextDescricao.getText().toString());
+                produto.setUnidade(editTextUnidade.getText().toString());
+                produto.setCodigoBarras(editTextCodigo.getText().toString());
+                if(!editTextPreco.getText().toString().equals("")){
+                    produto.setPreco(Double.parseDouble(editTextPreco.getText().toString()));
+                }
+                Bitmap imagem = ((BitmapDrawable)imageViewFoto.getDrawable()).getBitmap();
+
+                produto.setFoto(Base64Util.encodeTobase64(imagem));
+                produto.save();
+
+                Snackbar.make(view,"Produto alterado com sucesso !",Snackbar.LENGTH_SHORT).show();
+
             }
         });
 
@@ -69,6 +115,17 @@ public class EditarProdutoActivity extends BaseActivity {
 
                 Log.d("BARCODE", "selecionado-->"+barcode);
 
+                produto = Query.one(Produto.class,"select * from produto where codigo_barra = ?",barcode).get();
+                if(produto!=null){
+
+                    editTextDescricao.setText(produto.getDescricao());
+                    editTextUnidade.setText(produto.getUnidade());
+                    editTextCodigo.setText(produto.getCodigoBarras());
+                    editTextPreco.setText(String.valueOf(produto.getPreco()));
+
+                    imageViewFoto.setImageBitmap(Base64Util.decodeBase64(produto.getFoto()));
+                }
+
 
             }
 
@@ -78,12 +135,47 @@ public class EditarProdutoActivity extends BaseActivity {
             }
         });
 
+    }
 
+    @OnClick(R.id.imageButtonGaleria)
+    public void onClickGaleria(){
+        imageInputHelper.selectImageFromGallery();
+    }
 
+    @OnClick(R.id.imageButtonCamera)
+    public void onClickCamera(){
+        imageInputHelper.takePhotoWithCamera();
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageInputHelper.onActivityResult(requestCode, resultCode, data);
+    }
 
+    @Override
+    public void onImageSelectedFromGallery(Uri uri, File imageFile) {
 
+        imageInputHelper.requestCropImage(uri, 100, 100, 0, 0);
+    }
 
+    @Override
+    public void onImageTakenFromCamera(Uri uri, File imageFile) {
+
+        imageInputHelper.requestCropImage(uri, 100, 100, 0, 0);
+    }
+
+    @Override
+    public void onImageCropped(Uri uri, File imageFile) {
+        try {
+            // getting bitmap from uri
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+            imageViewFoto.setImageBitmap(bitmap);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
